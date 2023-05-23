a9ccf61fb7ab5002d5936d53bbf7a0dcfb292161
YammyShep <jdbuelow@gmail.com>
2023-05-23 16:52:28 -0500

    Camera rotation implemented

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -4,6 +4,9 @@ extends Camera3D
 
 @export var rotationpoint: Node3D
 @export var dragModifier: float = 0.01
+@export var rotationModifier: float = 0.01
+@export var minAngle: float = 6
+@export var maxAngle: float = 174
 
 var dragCamera: bool = false
 var rotateCamera: bool = false
@@ -14,6 +17,9 @@ func _ready():
 	# Check for invalid rotation point parameter
 	if (! is_instance_valid(rotationpoint)):
 		push_error("Camera controller on node %s has an invalid or unset rotation point!" % get_path())
+		
+	look_at(rotationpoint.position)
+	rotationpoint.rotation = rotation
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,9 +49,33 @@ func _input(event):
 				
 	if event is InputEventMouseMotion:
 		if dragCamera:
-			var offset := Vector3(-event.relative.x, -event.relative.y, 0)
+			var offset := Vector3(-event.relative.x, event.relative.y, 0)
 			translate_object_local(offset * dragModifier)
 			rotationpoint.translate_object_local(offset * dragModifier)
 		
 		if rotateCamera:
-			pass
+			var basis := global_transform.basis
+			var origin := global_transform.origin - rotationpoint.position
+			
+			# Rotate on x axis
+			basis = basis.rotated(Vector3.UP, -event.relative.x * rotationModifier)
+			origin = origin.rotated(Vector3.UP, -event.relative.x * rotationModifier)
+			
+			# Rotate on y axis
+			var yLimited: bool = false
+			var forward := -transform.basis.z
+			if forward.angle_to(Vector3.UP) < (minAngle * PI / 180):
+				if -event.relative.y >= 0:
+					yLimited = true
+			elif forward.angle_to(Vector3.UP) > (maxAngle * PI / 180):
+				if -event.relative.y <= 0:
+					yLimited = true
+			
+			if not yLimited:
+				basis = basis.rotated(global_transform.basis.x, -event.relative.y * rotationModifier)
+				origin = origin.rotated(global_transform.basis.x, -event.relative.y * rotationModifier)
+			
+			# Rebuild transform from basis and origin
+			global_transform = Transform3D(basis, rotationpoint.position + origin)
+			look_at(rotationpoint.position)
+			rotationpoint.rotation = rotation;
