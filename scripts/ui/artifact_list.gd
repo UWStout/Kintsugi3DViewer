aa9393954186130957970061b4dd650a4f5edc24
Jacob Buelow <jdbuelow@gmail.com>
2023-06-05 13:24:54 -0500

    Loading data remotely

--- scripts/ui/artifact_list.gd
+++ scripts/ui/artifact_list.gd
@@ -1,21 +1,9 @@
 extends Panel
 
 @export var artifact_button: PackedScene
+@export var remote_data_url: String
 
-var test_data = [
-	{
-		"name": "Test Object A",
-		"iconUrl": "https://i.imgur.com/j9tpg3M.jpg"
-	},
-	{
-		"name": "Test Object B",
-		"iconUrl": "https://i.imgur.com/godLY1r.jpg"
-	},
-	{
-		"name": "Test Object C",
-		"iconUrl": "https://i.imgur.com/Xp4NZCg.jpg"
-	}
-]
+var artifacts_data: Array
 
 func _ready():
 	hide_panel()
@@ -23,8 +11,8 @@ func _ready():
 		push_error("Artifact button not set!")
 
 func show_panel():
-	populate_list()
 	show()
+	load_remote_data()
 
 func hide_panel():
 	hide()
@@ -41,7 +29,21 @@ func clear_list():
 
 func populate_list():
 	clear_list()
-	for artifact_data in test_data:
+	for artifact_data in artifacts_data:
 		var button = artifact_button.instantiate()
 		%ArtifactList.add_child(button)
 		button.setup(artifact_data)
+
+func load_remote_data():
+	var req = HTTPRequest.new()
+	add_child(req)
+	req.request_completed.connect(_on_request_completed)
+	req.request(remote_data_url)
+
+func _on_request_completed(result, response_code, headers, body):
+	if response_code != 200:
+		push_error("Failed to retrieve remote URL!")
+	
+	var parsed_data = JSON.parse_string(body.get_string_from_utf8())
+	artifacts_data = parsed_data
+	populate_list()
