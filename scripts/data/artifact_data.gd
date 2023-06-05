e5339e1edddabec230d2d43a89159ad8db7564c7
Jacob Buelow <jdbuelow@gmail.com>
2023-06-05 16:25:37 -0500

    Test scene successfully loading remote gltf mesh

--- scripts/data/artifact_data.gd
+++ scripts/data/artifact_data.gd
@@ -3,12 +3,16 @@ class_name ArtifactData
 
 var name: String
 var iconUrl: String
-#TODO: Fields for GLTF loading
+var gltfUrl: String
 
 static func from_dict(data: Dictionary) -> ArtifactData:
 	var out_data = ArtifactData.new()
-	out_data.name = data.get("name")
-	out_data.iconUrl = data.get("iconUrl")
+	if data.has("name"):
+		out_data.name = data.get("name")
+	if data.has("iconUrl"):
+		out_data.iconUrl = data.get("iconUrl")
+	if data.has("gltfUrl"):
+		out_data.gltfUrl = data.get("gltfUrl")
 	return out_data
 
 func _to_string() -> String:
