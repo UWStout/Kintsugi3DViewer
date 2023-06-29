9c3a75ba270989846bb1f700015990b07179ea34
Jacob Buelow <jdbuelow@gmail.com>
2023-06-29 16:19:30 -0500

    Cleanup unused scripts and assets

--- test-dynamic-load.gd
+++
@@ -1,5 +0,0 @@
-extends MeshInstance3D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_active_material(0).load(self) # assume active material is an HTTPMaterial
