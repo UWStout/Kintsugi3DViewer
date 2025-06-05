a8d076683d27ea0345f06e174f714f65e201f1f1
Melissa <116676508+mkosh11@users.noreply.github.com>
2025-06-05 15:20:53 -0500

    final ui sizing solution

--- scripts/new_ui/ui_scaling.gd
+++ scripts/new_ui/ui_scaling.gd
@@ -5,13 +5,14 @@ var mac_resolution = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if (OS.get_name() == "macOS"):
-		ProjectSettings.set_setting("display/window/stretch/scale", mac_resolution)
-		ProjectSettings.save()
+	#Check if screen size is larger than double the default window size. Typically applies to Mac displays, but works on any high-resolution screen
+	if (DisplayServer.screen_get_size() >= Vector2i(2304, 1296)):
+		get_window().content_scale_factor = mac_resolution
+		DisplayServer.window_set_size(get_viewport().size * mac_resolution)
 	else:
-		ProjectSettings.set_setting("display/window/stretch/scale", default_resolution)
-		ProjectSettings.save()
-	print("SCALING SIZE: ", ProjectSettings.get_setting("display/window/stretch/scale"))
+		get_window().content_scale_factor = default_resolution
+		DisplayServer.window_set_size(get_viewport().size * default_resolution)
+	#print(DisplayServer.window_get_size())
 	#pass
 
 
