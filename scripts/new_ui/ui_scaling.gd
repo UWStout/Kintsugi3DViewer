de3b9ec120b1aad3037e1c834ccf1fc2ccdccfe1
MelissaPC <kosharekm@gmail.com>
2025-06-05 09:12:09 -0500

    ui scaling windows vs mac

--- scripts/new_ui/ui_scaling.gd
+++ scripts/new_ui/ui_scaling.gd
@@ -1,45 +1,20 @@
 extends Control
 
-var base_resolution = Vector2(1142, 648)
-var max_resolution = 1.5
-var max_scale_x
-var max_scale_y
+var default_resolution = 1
+var mac_resolution = 2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_window().connect("size_changed", Callable(self, "on_window_size_changed"))
-	max_scale_x = base_resolution.x * max_resolution
-	max_scale_y = base_resolution.y * max_resolution
-	unlock_ui_scale()
+	if (OS.get_name() == "macOS"):
+		ProjectSettings.set_setting("display/window/stretch/scale", mac_resolution)
+		ProjectSettings.save()
+	else:
+		ProjectSettings.set_setting("display/window/stretch/scale", default_resolution)
+		ProjectSettings.save()
+	print("SCALING SIZE: ", ProjectSettings.get_setting("display/window/stretch/scale"))
 	#pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func on_window_size_changed():
-	print("window size changed")
-	#update_ui_scale()
-	print("SIZE: ", self.size, "VS MAX SIZE: (", max_scale_x, ", ", max_scale_y)
-	if ((self.size.x >= max_scale_x) or (self.size.y >= max_scale_y)):
-		#pass
-		max_ui_scale()
-		print("ui scale bigger than 2")
-	elif (ProjectSettings.get_setting("display/window/stretch/scale") == max_resolution):
-		#pass
-		unlock_ui_scale()
-		print("unlocked")
-	else:
-		print("already unlocked, no need to unlock")
-	print("PROJECT SETTINGS: ", ProjectSettings.get_setting("display/window/stretch/scale"), ProjectSettings.get_setting("display/window/stretch/mode"))
-
-
-func max_ui_scale():
-	ProjectSettings.set_setting("display/window/stretch/mode", "disabled")
-	ProjectSettings.set_setting("display/window/stretch/scale", max_resolution)
-	ProjectSettings.save()
-func unlock_ui_scale():
-	ProjectSettings.set_setting("display/window/stretch/mode", "canvas_items")
-	ProjectSettings.set_setting("display/window/stretch/scale", 1)
-	ProjectSettings.save()
-	
