fa22641f36c1e5e5eac9d9aeff56cef9003197d7
Michael Tetzlaff <michaelt919@gmail.com>
2025-06-10 14:50:38 -0500

    Adjust UI scaling logic to handle 1440p and ultrawide correctly.

--- scripts/new_ui/ui_scaling.gd
+++ scripts/new_ui/ui_scaling.gd
@@ -5,8 +5,9 @@ var mac_resolution = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#Check if screen size is larger than double the default window size. Typically applies to Mac displays, but works on any high-resolution screen
-	if (DisplayServer.screen_get_size() >= Vector2i(2304, 1296)):
+	# Check if screen height is greater than 1440 pixels. Typically applies to Mac displays, but works on any high-resolution screen.  
+	# This should scale up for 4K displays but not 1440p or ultra-wide displays that are 1440 pixels or less vertically.
+	if (DisplayServer.screen_get_size().y > 1440):
 		get_window().content_scale_factor = mac_resolution
 		DisplayServer.window_set_size(get_viewport().size * mac_resolution)
 	else:
