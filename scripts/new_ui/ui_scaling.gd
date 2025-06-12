1af37471044120548073075c0b729cc12e322e94
MelissaKosharek <kosharekm@gmail.com>
2025-06-12 10:34:24 -0500

    merge correction

--- scripts/new_ui/ui_scaling.gd
+++ scripts/new_ui/ui_scaling.gd
@@ -6,16 +6,12 @@ var resolution
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#Check if screen size is larger than double the default window size. Typically applies to Mac displays, but works on any high-resolution screen
-	if (DisplayServer.screen_get_size() >= Vector2i(2304, 1296)):
-		get_window().content_scale_factor = high_resolution
-		DisplayServer.window_set_size(get_viewport().size * high_resolution)
-		resolution = "high"
 	# Check if screen height is greater than 1440 pixels. Typically applies to Mac displays, but works on any high-resolution screen.  
 	# This should scale up for 4K displays but not 1440p or ultra-wide displays that are 1440 pixels or less vertically.
 	if (DisplayServer.screen_get_size().y > 1440):
-		get_window().content_scale_factor = mac_resolution
-		DisplayServer.window_set_size(get_viewport().size * mac_resolution)
+		get_window().content_scale_factor = high_resolution
+		DisplayServer.window_set_size(get_viewport().size * high_resolution)
+		resolution = "high"
 	else:
 		get_window().content_scale_factor = default_resolution
 		DisplayServer.window_set_size(get_viewport().size * default_resolution)
