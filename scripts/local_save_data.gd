dfa05c8fc9fc7e513629e17e67d9cc45be069418
MelissaKosharek <kosharekm@gmail.com>
2025-06-26 13:56:50 -0500

    made test function for json file creation

--- scripts/local_save_data.gd
+++ scripts/local_save_data.gd
@@ -6,7 +6,8 @@ func _ready():
 	print("starting json file check")
 	if not _does_save_exist():
 		_create_save_file()
-		_init_save()
+		#_init_save()
+		_test_init() #currently in testing/prototyping stages, will use init save in final architecture
 
 
 
@@ -18,15 +19,21 @@ func _create_save_file():
 	var save = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.WRITE)
 	
 func _init_save():
-	var data_to_send = {"artifacts" : [{"name": "temp", "dir": "tempp" }, {"name": "temp1", "dir": "tempp1" }]}
-	var json_string = JSON.stringify(data_to_send)
+	var json_string = JSON.stringify({"artifacts" : []})
 	var json = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.WRITE)
 	json.store_string(json_string)
 	json.close()
-	var jsonr = JSON.new()
-	var error = jsonr.parse(json_string)
+	
+func _test_init():
+	#This writes and pulls dummy data for testing purposes.
+	var data_to_send = {"artifacts" : [{"name": "temp", "localDir": "tempp" }, {"name": "temp1", "localDir": "tempp1" }]}
+	var json_string = JSON.stringify(data_to_send)
+	var json = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.WRITE)
+	json.store_string(json_string)
+	json.get_as_text()
+	var error = json.parse(json_string)
 	if error == OK:
-		var data_received = jsonr.data
+		var data_received = json.data
 		print(json_string)
 		if typeof(data_received) == TYPE_ARRAY:
 			print(data_received) # Prints the array.
@@ -34,4 +41,7 @@ func _init_save():
 			print("Unexpected data")
 			print(data_received) # Prints the array.
 	else:
-		print("JSON Parse Error: ", jsonr.get_error_message(), " in ", json_string, " at line ", jsonr.get_error_line())
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		json.close()
+func _save_model(name, dir) -> void:
+	var data_to_send = {"name" : name, "localDir" : dir}
