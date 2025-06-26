41ab1b91bcd78dbbfd4ea65875bb5b0b7900b227
MelissaPC <kosharekm@gmail.com>
2025-06-26 17:28:01 -0500

    initialization for local save success
    need to keep working on save model & better utilize godot dictionaries

--- scripts/local_save_data.gd
+++ scripts/local_save_data.gd
@@ -6,8 +6,9 @@ func _ready():
 	print("starting json file check")
 	if not _does_save_exist():
 		_create_save_file()
-		#_init_save()
-		_test_init() #currently in testing/prototyping stages, will use init save in final architecture
+		_init_save()
+		#_test_init() #only for testing purposes
+		_save_model("dummy", "data")
 
 
 
@@ -19,29 +20,54 @@ func _create_save_file():
 	var save = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.WRITE)
 	
 func _init_save():
-	var json_string = JSON.stringify({"artifacts" : []})
+
+	var json_string = JSON.stringify({"artifacts" : [{"name" : "Temp", "localDir" : "temp"}]})
 	var json = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.WRITE)
 	json.store_string(json_string)
 	json.close()
 	
 func _test_init():
 	#This writes and pulls dummy data for testing purposes.
-	var data_to_send = {"artifacts" : [{"name": "temp", "localDir": "tempp" }, {"name": "temp1", "localDir": "tempp1" }]}
-	var json_string = JSON.stringify(data_to_send)
-	var json = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.WRITE)
-	json.store_string(json_string)
-	json.get_as_text()
-	var error = json.parse(json_string)
+	var data_to_send = {"artifacts" : [{"num" : 1, "num2" : 2}, {"other num1" : 3, "other num2" : 4}]}
+	var json_string = JSON.stringify(data_to_send, "\t")
+	var file = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.READ_WRITE)
+	file.store_string(json_string)
+	var json = JSON.new()
+	var error = json.parse(file.get_as_text())
+	print(json.data)
 	if error == OK:
 		var data_received = json.data
-		print(json_string)
-		if typeof(data_received) == TYPE_ARRAY:
+		if typeof(data_received) == TYPE_DICTIONARY:
+			print("success")
 			print(data_received) # Prints the array.
 		else:
 			print("Unexpected data")
 			print(data_received) # Prints the array.
 	else:
 		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
-		json.close()
+	file.close()
+	
 func _save_model(name, dir) -> void:
 	var data_to_send = {"name" : name, "localDir" : dir}
+	var file = FileAccess.open("user://" + _LOCAL_SAVE_FILE, FileAccess.READ_WRITE)
+	var original_json = JSON.new()
+	var parse = original_json.parse(file.get_as_text())
+	#TODO
+		#turn current file into dictionary
+		#add data_to_send to dictionary
+	var json_string #stringify new dictionary
+	file.store_string(json_string)
+	
+	var json = JSON.new()
+	var error = json.parse(file.get_as_text())
+	print(json.data)
+	if error == OK:
+		var data_received = json.data
+		if typeof(data_received) == TYPE_DICTIONARY:
+			print("success")
+		else:
+			print("not recognized as a dictionary")
+			print(typeof(data_received))
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+	file.close()
