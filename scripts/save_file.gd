453a1d08388bd38d4ab4d716ee73dfaebdc6cd91
MelissaKosharek <kosharekm@gmail.com>
2025-07-11 16:30:35 -0500

    missed save

--- scripts/save_file.gd
+++ scripts/save_file.gd
@@ -1,8 +1,12 @@
 extends Button
 
-@export var LineText: LineEdit
+@export var fileName: LineEdit
 
 
 
 func _on_pressed() -> void:
-	pass # Replace with function body.
+	pass
+	#fileName.get_text()
+	#pull current filepath
+	#how to? -> global variable? would have to mess around with local vs cloud, probably more work than its worth
+	#		pass the variable directly to this script: open popup -> 
